fef57930b53571bab06b247296ea1b78b2825569
Victor Mendes <victormendes13@gmail.com>
2023-10-13 01:42:50 -0300

    Create word-list.js + fix src_script.js-index.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 const keyboardDiv = document.querySelector(".keyboard");
 
+const getRandomWord = () => {
+    
+}
+
 // Creating keyboard buttons
 for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
